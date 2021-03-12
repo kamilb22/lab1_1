3835834dc81fe015939eae0399d6526b32d3f99e
kamil <222574@edu.p.lodz.pl>
2021-03-12 17:10:07 +0100

    Adding a product class.

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.util.Date;
+
+public class Product {
+    private String id;
+
+    private String type;
+
+    private String name;
+
+    private Date snapshotDate;
+}
